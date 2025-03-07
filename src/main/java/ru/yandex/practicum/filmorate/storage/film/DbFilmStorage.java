7c8bfe2d31ea4747d606e519fe0a3399e52e1cf9
qwuadr1xx <qwuadrixx@gmail.com>
2025-03-07 19:16:35 +0500

    /bugfix изменить SQL запрос

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/DbFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/DbFilmStorage.java
@@ -49,7 +49,7 @@ public class DbFilmStorage implements FilmStorage {
             SELECT f.id, f.name, f.description, f.release_date, f.duration, f.mpa_rating_id, m.name AS mpa_rating_name
             FROM films AS f
             INNER JOIN likes AS l ON f.id = l.film_id
-            INNER JOIN mpa_ratings AS m ON f.mpa_rating_id = m.id
+            LEFT JOIN mpa_ratings AS m ON f.mpa_rating_id = m.id
             """;
 
     private static final String GET_LIKED_FILMS_GROUP_AND_SORT = """
