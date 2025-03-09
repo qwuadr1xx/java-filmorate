8aaa88450dc7326fff93e0fb984a42a49f1dacb8
qwuadr1xx <qwuadrixx@gmail.com>
2025-03-09 15:07:54 +0500

    /bugfix изменить запуск тестов

--- src/test/java/ru/yandex/practicum/filmorate/UserDbTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserDbTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.film.DbFilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.DbUserStorage;
 
 import java.time.LocalDate;
@@ -14,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
 @AutoConfigureTestDatabase
-@Import(DbUserStorage.class)
+@Import({DbUserStorage.class, DbFilmStorage.class})
 class UserDbTest {
 
     @Autowired
